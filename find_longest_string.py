796fbbc014e82b419c4ba3ddcbb64a861d070974
SakshamV19 <91875104+SakshamV19@users.noreply.github.com>
2024-06-07 15:37:42 +0530

    Update find_longest_string.py

--- find_longest_string.py
+++ find_longest_string.py
@@ -1,4 +1,4 @@
-"""List: Find Longest String ( ** Interview Question)
+"""List: Find Longest String 
 Write a Python function called find_longest_string that takes a list of strings as an input and returns the longest string in the list. The function should iterate through each string in the list, check its length, and keep track of the longest string seen so far. Once it has looped through all the strings, the function should return the longest string found.
 
 
