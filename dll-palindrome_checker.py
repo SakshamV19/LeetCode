0d4ed9569ec453d76ac46f4368a9eddaff3dbef3
SakshamV19 <91875104+SakshamV19@users.noreply.github.com>
2024-06-07 16:56:16 +0530

    Update dll-palindrome_checker.py

--- dll-palindrome_checker.py
+++ dll-palindrome_checker.py
@@ -1,4 +1,4 @@
-"""DLL: Palindrome Checker ( ** Interview Question)
+"""DLL: Palindrome Checker 
 Write a method to determine whether a given doubly linked list reads the same forwards and backwards.
 
 For example, if the list contains the values [1, 2, 3, 2, 1], then the method should return True, since the list is a palindrome.
