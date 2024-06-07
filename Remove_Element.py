b53f4caa94233110218c7b9262c669c868c14932
SakshamV19 <91875104+SakshamV19@users.noreply.github.com>
2024-06-07 15:38:08 +0530

    Update Remove_Element.py

--- Remove_Element.py
+++ Remove_Element.py
@@ -1,4 +1,4 @@
-"""List: Remove Element ( ** Interview Question)
+"""List: Remove Element 
 Given a list of integers nums and an integer val, write a function remove_element that removes all occurrences of val in the list in-place and returns the new length of the modified list.
 
 The function should not allocate extra space for another list; instead, it should modify the input list in-place with O(1) extra memory.
